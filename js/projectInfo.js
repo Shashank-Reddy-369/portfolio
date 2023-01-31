51b94f861e8b12f4d9df80779e8cb43bb3595352
Shashank-Reddy-369 <38601209+Shashank-Reddy-369@users.noreply.github.com>
2023-01-31 15:49:45 -0700

    Initial Commit

---
+++ js/projectInfo.js
@@ -0,0 +1,106 @@
+const Projects = {
+    Details:{//adding in opposite-chronological order, so it's easy to add later    
+        Template: {
+            ID: "Template",
+            Name: "",
+            SmallDescription: "",
+            LargeDescription: "",
+            DownloadLink: "",
+            EmbedVideoLinkID: "",
+            NoOfProjectImages: 1
+        },
+        Starbucks: {
+            ID: "Starbucks",
+            Name: "ImageTracking AR",
+            SmallDescription: "webAR, threeJS, A-Frame",
+            LargeDescription: "Developed a marketing experience for image tracking in AR using the A-Frame AR engine.",
+            EmbedVideoLinkID: "gqXHuxI--3s",
+            NoOfProjectImages: 0
+        },
+        GreenScreenRendering: {
+            ID: "GreenScreenRendering",
+            Name: "AR Hologram",
+            SmallDescription: "webAR, threeJS, A-Frame",
+            LargeDescription: "Developed a marketing experience for generating holograms in AR from green screen videos using the A-Frame AR engine.",
+            DownloadLink: "",
+            EmbedVideoLinkID: "eI-yJVf7sEs",
+            NoOfProjectImages: 0
+        },
+        VirtualBooth: {
+            ID: "VirtualBooth",
+            Name: "VirtualBooth Rendering in AR",
+            SmallDescription: "webAR, threeJS, A-Frame",
+            LargeDescription: "Developed an AR web application using ThreeJS that spawns a human-scale virtual booth. Users can walk around the booth with their mobile phones to explore and interact with various components, such as videos and images. This demo was presented at the ASU-GSV annual summit held in March 2022 and logged over 3,000+ views in two days.",
+            EmbedVideoLinkID: "ZVA_I5x0mwo",
+            NoOfProjectImages: 0
+        },
+        Mario: {
+            ID: "Mario",
+            Name: "Mario 2D",
+            SmallDescription: "Published Android Game, C#, Unity",
+            LargeDescription: "Developed a Android game in Unity that is a replica of Nintendo-Mario game with multiple level selection and character animations. Hosted it on the Google Play Store with over 100 downloads.",
+            EmbedVideoLinkID: "AXQu9mVzHEI",
+            NoOfProjectImages: 0
+        },        
+        PVS: {
+            ID: "PVS", 
+            Name: "Sailing Simulation in VR",
+            SmallDescription: "Meta Quest, WebGL",
+            LargeDescription: "Built a Virtual-Reality sailing simulation game in Unity-XR framework. Developed libraries to simulate the buoyancy forces of the water on canoes and built a physics-based hand interaction system. Hosted it on the Meta Quest store which has over 200 downloads.",
+            EmbedVideoLinkID: "QB0eaZW36No",
+            NoOfProjectImages: 0
+        },
+        Geodesign: {
+            ID: "Geodesign",
+            Name: "Geodesign",
+            SmallDescription: "Unity 2D, WebGL, Mobile",
+            LargeDescription: "Developed a 2D web application in Unity that is intended to replicate an authentic geo design workshop, in which students assume the role of a stakeholder tasked with collaborating with other stakeholders to reach a design decision that will benefit all stakeholders and their constituents. This app is an integral part of the Geography Information Science (GIS451) course at Arizona State University. Used Mapbox API support to render real-time map data and authentically validate city planning for students.",
+            EmbedVideoLinkID: "82j-mWC8e0A",
+            NoOfProjectImages: 0
+        },
+        MadBirds: {
+            ID: "MadBirds",
+            Name: "MadBirds 2D",
+            SmallDescription: "Published Android Game, Unity",
+            LargeDescription: "Developed a Android application in Unity similar to the game Angry-Birds with multiple-level design and character animations. Hosted the application on the Google Play Store, which has over 300 downloads.",
+            DownloadLink: "",
+            EmbedVideoLinkID: "Mu8Fx3dB3p8",
+            NoOfProjectImages: 0
+        },
+        MiniGolf: {
+            ID: "MiniGolf",
+            Name: "VR Golf",
+            SmallDescription: "Quest VR, Unity 3D",
+            LargeDescription: "Developed a 3D golf game for the Quest platform using Unity. This game keeps track of the score, current level, and increases the difficulty as the user gains more experience in the game. <br> Wrote Surface Shaders from scratch in Cg/HLSL for Quest VR with runtime editable patterns and configured for most optimized Unity3D settings.",
+            DownloadLink: "",
+            EmbedVideoLinkID: "wei5IGkmHN4",
+            NoOfProjectImages: 0
+        },
+        ElevateAR: {
+            ID: "ElevateAR",
+            Name: "ElevateAR",
+            SmallDescription: "ARKit, iOS, AR",
+            LargeDescription: "Created a 3D portal AR experience that fits within the context of an Elevator. The content inside the portal adjusts according to the movement of the Elevator. For example, when the elevator moves up from one floor to the next, we see the camera move upward from a giraffe's tail to it's neck. <br> Developed this experience in ARKit and deployed it for the iOS platform. Used ARKit's native API's to compute the altitude and other related parameters to support this functionality.",
+            EmbedVideoLinkID: "oWgg2ReUznI",
+            NoOfProjectImages: 0
+        },
+        Dreamscape: {
+            ID: "Dreamscape",
+            Name: "Aliens in AR",
+            SmallDescription: "webAR, threeJS, A-Frame",
+            LargeDescription: "Built a webAR marketing experience for rendering alien creatures in AR using threeJS and A-Frame engines. This experience was presented at the ASU-GSV summit held in March 2022 and logged over 2000+ views in 2 days.",
+            DownloadLink: "",
+            EmbedVideoLinkID: "H6n57XR1b4E",
+            NoOfProjectImages: 0
+        },
+        GeneticsPOC: {
+            ID: "GeneticsPOC",
+            Name: "Genetics POC",
+            SmallDescription: "WebGL, Js, Unity",
+            LargeDescription: "The Genetics POC is a biological simulation where a student learns about the genotypes of animals and how traits are mixed and passed from one animal to their offspring. In this experience the student takes on the role of a farmer tasked with a breeding a pig with a specific genetic code. This experience is part of the circulum of Animal Physiology(BIO360) course at Arizona State University. Developed this experience in Unity 2D and deployed it for the web platform.",
+            DownloadLink: "",
+            EmbedVideoLinkID: "b4EXL1AYOG8",
+            NoOfProjectImages: 0
+        }
+    }
+};
